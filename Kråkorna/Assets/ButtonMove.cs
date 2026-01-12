bedf56c794d93e40bb7d874a3616c4f333d0146a
JonathanJyrwall <Jonathan.jyrwall@outlook.com>
2026-01-12 19:11:43 +0100

    fixade animationerna

--- Kråkorna/Assets/ButtonMove.cs
+++ Kråkorna/Assets/ButtonMove.cs
@@ -5,10 +5,16 @@ public class ButtonMove : MonoBehaviour
     public float amplitude = 0.2f;
     public float speed = 1f;
     private float startY;
+    public float tiltAngle = 5f;    
+    public float tiltSpeed = 2f;
+    private RectTransform rectTransform;
+    private Quaternion startRotation;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         startY = transform.position.y;
+        rectTransform = GetComponent<RectTransform>();
+        startRotation = rectTransform.localRotation;
     }
 
     // Update is called once per frame
@@ -16,8 +22,9 @@ public class ButtonMove : MonoBehaviour
     {
         float newY = startY + Mathf.Sin(Time.time * speed) * amplitude;
         transform.position = new Vector3(transform.position.x, newY, transform.position.z);
-        float rotation = -55 + newY * 0.1f ;
-        transform.eulerAngles = Vector3.forward * rotation;
+        float tilt = Mathf.Sin(Time.time * tiltSpeed) * tiltAngle;
+        rectTransform.localRotation = startRotation * Quaternion.Euler(0f, 0f, tilt);
+
 
     }
 }
