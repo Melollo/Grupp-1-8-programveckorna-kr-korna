3485b86e41cccccbd08b3b5952816ff686b0a3ad
lukaslundback <lukas.lundback@elev.ga.lbs.se>
2026-01-12 15:17:44 +0100

    litepuzzel av melvin

--- Kråkorna/Assets/script/CollissionDetection.cs
+++ Kråkorna/Assets/script/CollissionDetection.cs
@@ -5,16 +5,43 @@ public class CollissionDetection : MonoBehaviour
 
     public CodeCheckerScript codeCheckerScript;
 
+    public GameObject isGreen;
+    public GameObject isRed;
+    public GameObject trueGreen;
+    public GameObject trueYellow;
+
+    public bool colliding = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
+        isGreen.SetActive(false);
+        isRed.SetActive(false);
+        trueGreen.SetActive(false);
+        trueYellow.SetActive(false);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (colliding)
+        {
+            isGreen.SetActive(true);
+            isRed.SetActive(false);
+            trueGreen.SetActive(true);
+            trueYellow.SetActive(false);
+        }
+        else
+        {
+            isGreen.SetActive(false);
+            isRed.SetActive(true);
+            trueGreen.SetActive(false);
+            trueYellow.SetActive(true);
+        }
+
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -22,12 +49,15 @@ public class CollissionDetection : MonoBehaviour
         if (collision.gameObject.CompareTag("trueBlock"))
         {
             SolvePuzzle();
+            colliding = true;
         }
     }
-
-    void OnCollissionExit2D()
+    void OnCollisionExit2D(Collision2D collision)
     {
-
+        if (collision.gameObject.CompareTag("trueBlock"))
+        {
+            colliding = false;
+        }
     }
 
     void SolvePuzzle()
